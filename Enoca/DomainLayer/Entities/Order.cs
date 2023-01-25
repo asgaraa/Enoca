8c742ae1c8f97a58d223f1478bf519b3f782bca2
asgaraa <asgaraa@users.noreply.github.com>
2023-01-25 18:04:13 +0400

    CreatedProject

---
+++ Enoca/DomainLayer/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Entities
+{
+    public class Order:BaseEntity
+    {
+        public string FirmaId { get; set; }
+        public Firma Firma { get; set; }
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ Enoca/DomainLayer/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using DomainLayer.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Entities
+{
+    public class Product:BaseEntity
+    {
+        public string FirmaId { get; set; }
+        public Firma Firma { get; set; }
+        public string Name { get; set; }
+        public bool Stok { get; set; }
+        public double Price { get; set; }
+    }
+}
